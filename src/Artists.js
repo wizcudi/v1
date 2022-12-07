114060080315c76631d21bdcdf8f6b8a8adb6091
Tony Saint-Amour <tonysaint-amour@Spooky-Mac.local>
2022-12-07 16:40:15 -0500

    added some basic function to Artist Directory

--- src/Artists.js
+++ src/Artists.js
@@ -1,8 +1,22 @@
 
 
+import ARTIST_DB from "./ARTIST_DB"
+
 function Artists(){
     return(
-        <div>Artists</div>
+        <div className="Artists">
+            {ARTIST_DB.map(artist => {
+                return(
+                    <div className="Artists__Card">
+                        <h2>{artist.name}</h2>
+                        <p>{artist.city}</p>
+                        <p>{artist.genre}</p>
+                        <p>{artist.bio}</p>
+                        <button>More Details</button>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 export default Artists;
