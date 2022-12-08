2f35452330d57906d8431f4b6f5a30f7b26fcdd9
Tony Saint-Amour <tonysaint-amour@Spooky-Mac.local>
2022-12-08 17:41:09 -0500

    12/8

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Directory from "./Directory";
 import Artists from "./Artists";
 import Venues from "./Venues";
 import Promoters from "./Promoters";
-import ArtistDetails from "./ArtistDetails";
 
 import { Routes, Route } from 'react-router-dom'
 
@@ -17,7 +16,6 @@ function App() {
         <Route path="/artists" element={<Artists />} />
         <Route path="/venues" element={<Venues />} />
         <Route path="/promoters" element={<Promoters />} />
-        <Route path="/artist/:id" element={<ArtistDetails />} />
       </Routes>
       
     </div>
--- src/Artists.js
+++ src/Artists.js
@@ -1,31 +1,115 @@
-import { Link } from 'react-router-dom'
+
+
 import ARTIST_DB from "./ARTIST_DB"
-import ArtistDetails from "./ArtistDetails"
+import { useState, useEffect } from 'react';
 
-function Artists(){
+function Artists() {
+
+
+  // Initialize the state with an empty array
+  const [artists, setArtists] = useState([]);
+  // Initialize the state for the selected artist with null
+  const [selectedArtist, setSelectedArtist] = useState(null);
+
+
+
+  // When the component is mounted, populate the state with the artist details
+  // And handle side effects
+  useEffect(() => {
+    setArtists(ARTIST_DB);
+  }, []);
+
+
+
+  // Map through the ARTIST_DB Array and return a component for each item
+  const ArtistMap = artists.map(artist => {
     return(
-        <div className="Artists">
-            {ARTIST_DB.map(artist => {
-                return(
-                    <div className="Artists__Card" key={artist.id}>
-                        
-                        <ArtistDetails
-                            name={artist.name}
-                            city={artist.city}
-                            genre={artist.genre}
-                            bio={artist.bio}
-                        />
-
-                        <Link to={`/artist/${artist.id}`}><button>More Details</button></Link>
-                        
-
-                    </div>
-                )
-            })}
-        </div>
+      <div className="Artists__Card" key={artist.id}>
+        <h2>{artist.name}</h2>
+        <p>{artist.city}</p>
+        <p>{artist.genre}</p>
+        <p>{artist.bio}</p>
+    
+        {/* // Add a button to select the artist */}
+        <button onClick={() => setSelectedArtist(artist)}>
+          More Details
+        </button>
+      </div>
     )
+  });
+
+
+
+  // Conditionally render the details for the selected artist
+  let artistDetails;
+  if (selectedArtist) {
+    artistDetails = (
+      <div>
+        <h2>{selectedArtist.name}</h2>
+        <p>{selectedArtist.city}</p>
+        <p>{selectedArtist.genre}</p>
+        <p>{selectedArtist.bio}</p>
+      </div>
+    );
+  }
+
+
+
+  return(
+    <div className="Artists">
+      {ArtistMap}
+      {artistDetails}
+    </div>
+  )
 }
+
+
+
 export default Artists;
+
+
+
+
+
+
+
+
+// function Artists() {
+//   // Initialize the state with an empty array
+//   const [artistDetails, setArtistDetails] = useState([]);
+
+//   // When the component is mounted, populate the state with the artist details
+//   useEffect(() => {
+//     setArtistDetails(ARTIST_DB);
+//   }, []);
+
+//   // Map through the artist details and return a component for each one
+//   const ArtistMap = artistDetails.map(artist => {
+//     return(
+//       <div className="Artists__Card" key={artist.id}>
+//         <ArtistDetails
+//           name={artist.name}
+//           city={artist.city}
+//           genre={artist.genre}
+//           bio={artist.bio}
+//         />
+//         <Link to={`/artist/${artist.name}`}>
+//             <button>More Details</button>
+//         </Link>
+//       </div>
+//     )
+//   });
+
+//   return(
+//     <div className="Artists">
+//       {ArtistMap}
+//     </div>
+//   )
+// }
+
+
+
+// export default Artists;
 
 
 
